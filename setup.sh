81c1446adf49e44a40770f0be8d47908059dcff5
Thoum <indiofish@gmail.com>
2015-09-28 23:24:21 +0900

    changed setup script

--- setup.sh
+++ setup.sh
@@ -5,9 +5,10 @@ echo "This script replaces the current vim settings!"
 rm -rf ~/.vimrc
 rm -rf ~/.vim/
 cp .vimrc ~/
+mkdir ~/.vim
+cp -r colors ~/.vim/
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
 
 vim +PlugInstall +qall 
-cp -r colors ~/.vim/
