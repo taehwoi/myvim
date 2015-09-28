6273bfcddf3ca1790400c5d183039a0a89ab8def
Thoum <indiofish@gmail.com>
2015-09-28 19:24:39 +0900

    added setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+echo "This script replaces the current vim settings!"
+
+rm -rf ~/.vimrc
+rm -rf ~/.vim/
+cp .vimrc ~/
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
+
+vim +PlugInstall +qall 
