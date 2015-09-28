d1838be5b92a0bed5a7c3417a577fde7373e10ed
Thoum <indiofish@gmail.com>
2015-09-28 23:26:19 +0900

    setup opens README.md while installing plugins

--- setup.sh
+++ setup.sh
@@ -11,4 +11,4 @@ cp -r colors ~/.vim/
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
 
-vim +PlugInstall +qall 
+vim ./README.md +PlugInstall +qall 
