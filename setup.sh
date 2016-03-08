bedeb495ea561c922c5a58a6130b448731f4891e
thoum <indiofish@gmail.com>
2016-03-08 20:45:45 +0900

    clean up

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ rm -rf ~/.vim/
 cp .vimrc ~/
 mkdir ~/.vim
 cp -r colors ~/.vim/
-cp -r ./.vim/syntax/ ~/.vim/
+cp -r syntax ~/.vim/
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
